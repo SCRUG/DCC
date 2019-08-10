32dcf1a39af88938b1f4698dbf78620f4cc85937
Andres Gonzalez <andres_m113@hotmail.com>
2019-08-10 14:12:15 -0600

    visualizaciones

--- SICOP/ui.r
+++ SICOP/ui.r
@@ -8,17 +8,35 @@ contenidoOpcion1 <- fluidPage(
   )
 )
 
+contenidoOpcion2 <- fluidPage(
+  fluidRow(
+    box(width = "100%", title = "Monto Total en Dolares de compras por Clasificacion", solidHeader = TRUE, status = "primary",
+        plotOutput("grafico_visualizacion_usd"))
+  )
+)
+
+contenidoOpcion3 <- fluidPage(
+  fluidRow(
+    box(width = "100%", title = "Monto Total en Colones de compras por Clasificacion", solidHeader = TRUE, status = "primary",
+        plotOutput("grafico_visualizacion_crc"))
+  )
+)
+
 #Interfaz
 ui <- (dashboardPage(
   header = dashboardHeader(title = "SICOP"),
   body = dashboardBody(
     tabItems(
-      tabItem(tabName = "Opcion1", contenidoOpcion1)
+      tabItem(tabName = "Opcion1", contenidoOpcion1),
+      tabItem(tabName = "Compras_USD", contenidoOpcion2),
+      tabItem(tabName = "Compras_CRC", contenidoOpcion3)
     )
   ),
   sidebar = dashboardSidebar(
     collapsed = FALSE, width = 200,
     sidebarMenu(id = "opciones",
-                menuItem(tabName = "Opcion1", text = "Opcion 1", icon = icon("home")))
+                menuItem(tabName = "Opcion1", text = "Datos", icon = icon("home")),
+                menuItem(tabName = "Compras_USD", text = "Compras USD", icon = icon("home")),
+                menuItem(tabName = "Compras_CRC", text = "Compras CRC", icon = icon("home")))
   )
 ))
